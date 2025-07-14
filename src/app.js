ab29084270352118b7826fb12a88a25bfe4a8b5b
fahadpj23 <fahadpj23@gmail.com>
2025-07-14 17:45:56 +0530

    latest

--- src/app.js
+++ src/app.js
@@ -1,40 +1,83 @@
+// require("dotenv").config();
+// const express = require("express");
+// const app = express();
+// const cors = require("cors");
+// const path = require("path");
+// const productRoutes = require("./routes/productRoutes");
+// const bannerRoutes = require("./routes/bannerRoutes");
+// const newArrivalRoutes = require("./routes/newArrivalRoutes");
+// const upcomingRoutes = require("./routes/upcomingRoutes");
+// const whatsappAdsRoutes = require("./routes/whatsappAdsRoutes");
+// const seriesRoutes = require("./routes/seriesRoutes");
+// const userRoutes = require("./routes/userRoutes");
+
+// app.use(express.json()); // Parse JSON requests
+// app.use(express.urlencoded({ extended: true }));
+// app.use(cors());
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true, // If you need to send cookies
+//   })
+// );
+
+// app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+// app.get("/", (req, res) => {
+//   res.send("Express.js Server is Running! 🚀");
+// });
+
+// app.use("/api/products", productRoutes);
+// app.use("/api/banner", bannerRoutes);
+// app.use("/api/newArrival", newArrivalRoutes);
+// app.use("/api/upcoming", upcomingRoutes);
+// app.use("/api/whatsappAds", whatsappAdsRoutes);
+// app.use("/api/series", seriesRoutes);
+// app.use("/api/user", userRoutes);
+
+// const PORT = process.env.PORT || 9000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
+
 require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const path = require("path");
-const productRoutes = require("./routes/productRoutes");
-const bannerRoutes = require("./routes/bannerRoutes");
-const newArrivalRoutes = require("./routes/newArrivalRoutes");
-const upcomingRoutes = require("./routes/upcomingRoutes");
-const whatsappAdsRoutes = require("./routes/whatsappAdsRoutes");
-const seriesRoutes = require("./routes/seriesRoutes");
-const userRoutes = require("./routes/userRoutes");
+// const productRoutes = require("./routes/productRoutes");
+// const bannerRoutes = require("./routes/bannerRoutes");
+// const newArrivalRoutes = require("./routes/newArrivalRoutes");
+// const upcomingRoutes = require("./routes/upcomingRoutes");
+// const whatsappAdsRoutes = require("./routes/whatsappAdsRoutes");
+// const seriesRoutes = require("./routes/seriesRoutes");
+// const userRoutes = require("./routes/userRoutes");
 
-app.use(express.json()); // Parse JSON requests
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true, // If you need to send cookies
-  })
-);
+// app.use(express.json()); // Parse JSON requests
+// app.use(express.urlencoded({ extended: true }));
+// app.use(cors());
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true, // If you need to send cookies
+//   })
+// );
 
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+// app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.get("/", (req, res) => {
   res.send("Express.js Server is Running! 🚀");
 });
 
-app.use("/api/products", productRoutes);
-app.use("/api/banner", bannerRoutes);
-app.use("/api/newArrival", newArrivalRoutes);
-app.use("/api/upcoming", upcomingRoutes);
-app.use("/api/whatsappAds", whatsappAdsRoutes);
-app.use("/api/series", seriesRoutes);
-app.use("/api/user", userRoutes);
+// app.use("/api/products", productRoutes);
+// app.use("/api/banner", bannerRoutes);
+// app.use("/api/newArrival", newArrivalRoutes);
+// app.use("/api/upcoming", upcomingRoutes);
+// app.use("/api/whatsappAds", whatsappAdsRoutes);
+// app.use("/api/series", seriesRoutes);
+// app.use("/api/user", userRoutes);
 
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
