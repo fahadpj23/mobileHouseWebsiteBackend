d0bec159e79ffe5a3a5abfa98d1046ba108dfd33
fahadpj23 <fahadpj23@gmail.com>
2025-07-15 10:39:58 +0530

    databaseconnection

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,29 @@
-module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
-  DB: "mobile_house_website",
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-};
+// module.exports = {
+//   HOST: "localhost",
+//   USER: "root",
+//   PASSWORD: "",
+//   DB: "mobile_house_website",
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000,
+//   },
+// };
+
+const mysql = require("mysql2/promise");
+
+const pool = mysql.createPool({
+  host:
+    process.env.DB_HOST ||
+    "database-1.chygea84cv0y.eu-north-1.rds.amazonaws.com",
+  user: process.env.DB_USER || "admin",
+  password: process.env.DB_PASSWORD || "FazilFajar159",
+  database: process.env.DB_NAME || "database-1",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+module.exports = pool;
