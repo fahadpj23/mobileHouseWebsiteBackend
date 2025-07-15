cb1df1eefb188566bc657a5ac6aa034b8cad3b57
fahadpj23 <fahadpj23@gmail.com>
2025-07-15 18:56:42 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -14,13 +14,14 @@ const userRoutes = require("./routes/userRoutes");
 app.use(express.json()); // Parse JSON requests
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true, // If you need to send cookies
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true, // If you need to send cookies
+//   })
+// );
+app.use(cors());
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
