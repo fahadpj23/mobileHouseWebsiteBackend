2cb2f40e731e16b8ed557c352337ef38ddf849a2
fahadpj23 <fahadpj23@gmail.com>
2025-07-15 10:47:37 +0530

    databaseconnection

--- src/config/db.js
+++ src/config/db.js
@@ -1,3 +1,17 @@
+module.exports = {
+  HOST: "database-1.chygea84cv0y.eu-north-1.rds.amazonaws.com",
+  USER: "admin",
+  PASSWORD: "FazilFajar159",
+  DB: "database-1",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
+
 // module.exports = {
 //   HOST: "localhost",
 //   USER: "root",
@@ -11,21 +25,3 @@
 //     idle: 10000,
 //   },
 // };
-
-const mysql = require("mysql2/promise");
-
-const pool = mysql.createPool({
-  host:
-    process.env.DB_HOST ||
-    "database-1.chygea84cv0y.eu-north-1.rds.amazonaws.com",
-  user: process.env.DB_USER || "admin",
-  password: process.env.DB_PASSWORD || "FazilFajar159",
-  database: process.env.DB_NAME || "database-1",
-  dialect: "mysql", // This is the critical line that was missing
-  port: process.env.DB_PORT || 3306,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
-
-module.exports = pool;
